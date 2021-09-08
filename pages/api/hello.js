1535a1163eca75e4f23226497ad0af90e27e8928
Vaclav Hrouda <wujido20@gmail.com>
2021-09-08 13:29:01 +0200

    code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const {Node: Logtail} = require("@logtail/js");
-const logtail = new Logtail("739qiFhRaZHxsTjZDvYxmnG3");
+// const logtail = new Logtail("739qiFhRaZHxsTjZDvYxmnG3");
 const logtail2 = new Logtail("ufzxfr43TGMkWyC3rZBbe2xn");
 
 export default function handler(req, res) {
-    logtail.info("logtail installed");
+    // logtail.info("logtail installed");
     logtail2.info("second logtail source")
     console.info("default console log")
     res.status(200).json({name: 'John Doe'})
